a2c4cbfdacf4588428e5cfb5d4c043c4d1620c24
jrgleason <jackiegleason@gmail.com>
2020-09-10 11:35:26 -0400

    Making it call the ts file directly to get that working first

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "index.mjs",
+  input: "ng/Application.ts",
   output: [
     {
       file:"./out/ng-demo.umd.js",
